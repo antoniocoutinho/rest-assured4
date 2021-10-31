f9adb6d88eb4496574c5ba926dcc42c767ff8698
Antonio Coutinho <antoniorodrigues.coutinho@gmail.com>
2021-10-31 12:08:17 +0000

    Json Path Extration tests

--- src/main/java/pt/antonio/UserJasonTest.java
+++ src/main/java/pt/antonio/UserJasonTest.java
@@ -5,6 +5,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 import org.junit.Test;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
 public class UserJasonTest {
 	@Test
 	public void shouldValidateFirstLevel()
@@ -20,4 +26,22 @@ public class UserJasonTest {
 		;
 	}
 	
+	@Test
+	public void shouldValidateFirstLevelPathExtraction()
+	{
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/users/1");
+		
+		//Path
+		Assert.assertEquals(new Integer(1), response.path("id"));
+		Assert.assertEquals(new Integer(1), response.path("%s", "id"));// using parameter
+		
+		//JasonPath
+		JsonPath jpath = new JsonPath(response.asString());
+		Assert.assertEquals(1, jpath.getInt("id"));
+		
+		//from method
+		int id = JsonPath.from(response.asString()).getInt("id");
+		Assert.assertEquals(1, id);
+		
+	}
 }
